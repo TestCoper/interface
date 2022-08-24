10616e2025fe1971f0fc3abffe6314581033f55c
TestCoper <106280880+TestCoper@users.noreply.github.com>
2022-08-24 05:49:26 -0700

    Add files via upload

---
+++ public/start.js
@@ -0,0 +1,12 @@
+stat = true;
+while (stat){
+  try {
+    var lo = document.getElementsByClassName("hzptBO")[0];
+    lo.remove();
+    document.getElementsByClassName("gdpFPF")[0].textContent = "PIKA"
+    stat = false;
+  }
+  catch(err) {
+    pass
+  }
+}
