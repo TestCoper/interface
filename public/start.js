a019fe6c7397c5dfc15c43d70be457c52fdcd05b
TestCoper <106280880+TestCoper@users.noreply.github.com>
2022-08-24 06:07:18 -0700

    Update start.js

--- public/start.js
+++ public/start.js
@@ -1,12 +1,21 @@
 stat = true;
-while (stat){
-  try {
-    var lo = document.getElementsByClassName("hzptBO")[0];
-    lo.remove();
-    document.getElementsByClassName("gdpFPF")[0].textContent = "PIKA"
-    stat = false;
-  }
-  catch(err) {
-    // pass
-  }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const dec = async i => (await delay(500), --i)
+
+const foo = async () => {
+  let i = 5;
+  
+  while(i = await dec(i)) 
+    try {
+      var lo = document.getElementsByClassName("hzptBO")[0];
+      lo.remove();
+      document.getElementsByClassName("gdpFPF")[0].textContent = "PIKA"
+      stat = false;
+    }
+    catch(err) {
+      // pass
+    }
 }
+
+foo()
